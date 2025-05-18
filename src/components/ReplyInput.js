74173d5e4c07ec4296822b795ad7768a6107ec98
Sandeep Murmu <sandeepmurmu99@gmail.com>
2025-05-19 01:09:32 +0530

    update cursor focus automatically on reply input

--- src/components/ReplyInput.js
+++ src/components/ReplyInput.js
@@ -1,10 +1,20 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 function ReplyInput({ replyTo }) {
   const [content, setContent] = useState(replyTo ? `@${replyTo} ` : "");
+  const replyTextRef = useRef(null);
+
+  useEffect(function () {
+    if (replyTextRef.current) {
+      const len = replyTextRef.current.value.length;
+      replyTextRef.current.focus();
+      replyTextRef.current.setSelectionRange(len, len);
+    }
+  }, []);
 
   return (
     <textarea
+      ref={replyTextRef}
       className="reply-input"
       placeholder="Add a comment..."
       rows={4}
